671fdf0bec27bdf4a03f6ea0557840e46fe8dae1
W.Xavier <wellington222688@gmail.com>
2024-09-16 09:39:15 -0300

    organizando exercicio 1

--- Code aleatorios/Codigos criados pelo celular/METODO ESTUDO/WX-Aprendizado/um_part1.py
+++ Code aleatorios/Codigos criados pelo celular/METODO ESTUDO/WX-Aprendizado/um_part1.py
@@ -6,13 +6,13 @@
    
    
    
-   #!part 1
-print(f"Hello, World!")
-print(f"Exercicio (1):Primeiro pedido:")
-print(f"Imprimindo todos os números de 0 a 9")
-print("Atenção aos detalhes")
-for i in range(10):
-    print(f"{'-'*10}({i}){'-'*10}")
-print(f"{'*'*23}")    
-
+def um_part1():  #!part 1
+    print(f"Hello, World!")
+    print(f"Exercicio (1):Primeiro pedido:")
+    print(f"Imprimindo todos os números de 0 a 9")
+    print("Atenção aos detalhes")
+    for i in range(10):
+        print(f"{'-'*10}({i}){'-'*10}")
+    print(f"{'*'*23}")    
+um_part1()
 
