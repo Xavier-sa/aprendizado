6c451c8bf11685d423e1e6c9a20100184b49751f
W.Xavier <wellington222688@gmail.com>
2024-10-04 18:30:11 -0300

    desafio

---
+++ APRENDER PYTHON/CODIGOS ALEATORIOS/desafio.py
@@ -0,0 +1,12 @@
+while True:
+    num = int(input("Informe um número inteiro:"))
+    resto = num %2
+
+    if resto == 0:
+        print("o número é par!")
+        break
+    else:
+        print("O número é ímpar!")
+        break
+        
+    
