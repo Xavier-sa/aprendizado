9e80714a10348183e54727c298f7b3e77de8908e
W.Xavier <wellington222688@gmail.com>
2024-10-15 10:19:33 -0400

    comandos git
    postando pra memorizar

---
+++ Code aleatorios/Codigos criados pelo celular/criação.py
@@ -0,0 +1,6 @@
+def criar():
+    print("Pode ser feito de duas maneiras:")
+    print(f"{'-'*30}")
+    print("Git clone https://...  .git")#copiar um repositorio
+    print("Git init")#que cria um novo repositorio local
+criar()    
