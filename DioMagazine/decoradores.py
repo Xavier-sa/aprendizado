b9ba4967eb08b42f2df0b27caec1bed4978c88a8
Xavier-sa <wellington222688@gmail.com>
2025-10-31 15:31:15 -0300

    feat: DECORADORES: salvando a funcao mensagem para mim

--- DioMagazine/decoradores.py
+++ DioMagazine/decoradores.py
@@ -3,3 +3,8 @@ def dizer_oi(nome):
 
 def incentivar_aprender(nome):
     return f"Oi {nome}, vamos aprender Pyhton juntos!"
+
+def mensagem_para_xavier(funcao_mensagem):
+    return funcao_mensagem("XAVIER")
+
+
